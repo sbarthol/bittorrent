c66045c171cef310c733f267fd26471567e6510b
Sacha Bartholme <sacha.bartholme@hotmail.com>
2020-09-12 01:11:51 +0200

    read from file

--- source/main.cpp
+++ source/main.cpp
@@ -6,12 +6,12 @@
 
 using namespace std;
 
-int main() {
+vector<char> get_bytes(string filename) {
 
 	ifstream is;
 	vector<char> bytes;
 
-	is.open("../puppy.torrent", ios::binary);
+	is.open(filename, ios::binary);
 	is.seekg(0, ios::end);
 	size_t filesize=is.tellg();
 	is.seekg(0, ios::beg);
@@ -20,6 +20,28 @@ int main() {
 	bytes.assign(istreambuf_iterator<char>(is),
 	                     istreambuf_iterator<char>());
 
-
-	bencode::Element e = ::bencode::parse(bytes);
+	return bytes;
 }
+
+int main() {
+
+	bencode::Element e = ::bencode::parse(get_bytes("../puppy.torrent"));
+}
+
+/*
+
+{
+	"info":
+	{
+		"pieces":"T?k?/?_(?Sh%???+]q'B?٠:????p"?j???1-g"?s(??V??=?h?ma?nF?2????ǩ?_?"2???'?wO??-;ע?ؑ??L&????0?D_9??	\??O?h,n5g?(??仑,?\߰?%??U????C>??df??",
+		"piece length":16384,
+		"name":"puppy.jpg",
+		"length":124234
+	},
+	"encoding":"UTF-8",
+	"creation date":1462355939,
+	"created by":"uTorrent/1870",
+	"announce":"udp://tracker.coppersurfer.tk:6969/announce"
+}
+
+*/
